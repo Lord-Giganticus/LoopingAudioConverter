5f202cf68582013ea2305c07eda044eca9699955
Lord-Giganticus <mrubins64@gmail.com>
2021-06-16 08:22:03 -0600

    update merge.bat

--- dist/merge.bat
+++ dist/merge.bat
@@ -1,14 +1,17 @@
 SET FROM=..\LoopingAudioConverter\bin\Release
 XCOPY /E /I %FROM%\*.exe windows
 XCOPY /E /I %FROM%\*.dll windows
+XCOPY /Y /E /I %FROM%\*.config windows
 XCOPY /E /I ..\LoopingAudioConverter\tools_win32 windows\tools
 XCOPY ..\LoopingAudioConverter\About.html windows
 XCOPY /E /I %FROM%\*.exe linux32
 XCOPY /E /I %FROM%\*.dll linux32
+XCOPY /Y /E /I %FROM%\*.config linux32
 XCOPY /E /I ..\LoopingAudioConverter\tools_linux-i686 linux32\tools
 XCOPY ..\LoopingAudioConverter\About.html linux32
 XCOPY /E /I %FROM%\*.exe linux64
 XCOPY /E /I %FROM%\*.dll linux64
+XCOPY /Y /E /I %FROM%\*.config linux64
 XCOPY /E /I ..\LoopingAudioConverter\tools_linux-amd64 linux64\tools
 XCOPY ..\LoopingAudioConverter\About.html linux64
 @PAUSE
